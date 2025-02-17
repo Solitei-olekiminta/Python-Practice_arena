07bf650c1bf6e8df6c4bc67f0dac107580574176
solitei-peter <162104196+Solitei-olekiminta@users.noreply.github.com>
2025-02-17 16:23:55 +0300

    Create make_pizza.py
    how to import the function on to the program

---
+++ make_pizza.py
@@ -0,0 +1,5 @@
+import pizza
+""""Pizza if the name of the file where we have stored out function"""
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
