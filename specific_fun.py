4e50e2b1d2b765bcad02e6c925972f411334135a
solitei-peter <162104196+Solitei-olekiminta@users.noreply.github.com>
2025-02-17 16:30:02 +0300

    Create specific_fun.py
    demonstration of how to import a specific function in a number of functions

---
+++ specific_fun.py
@@ -0,0 +1,6 @@
+from pizza import make_pizza as mp
+""""Importation of a specific function from a number of functions"""
+
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
